8fa399d32f658286ced8474800e62ef7cb89ef3e
Unknown <josu.mtnz@gmail.com>
2017-10-11 21:11:02 +0200

    Ejercicio 5 terminado.

---
+++ 05_dia_semana/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int var;
+
+        do {
+            System.out.print("Escribe un número del 1 al 7: ");
+            var = Integer.parseInt(br.readLine());
+            System.out.println(var);
+        } while (!(var >=1 && var <=7));
+
+        switch (var) {
+
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Míercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+        }
+    }
+}
