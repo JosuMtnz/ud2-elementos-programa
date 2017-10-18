1f7c7156415b349040e97cec56d52ffac029126b
Unknown <josu.mtnz@gmail.com>
2017-10-18 21:51:00 +0200

    Ejercicio 11 terminado.

---
+++ 11_acertar_numero/src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Random;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // Inicializar variables.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Random r = new Random();
+        int aleatorio = r.nextInt(100);
+        int respuestaint = 0;
+        String respuesta = "";
+
+        // Mostrar numero generado.
+        //System.out.println(aleatorio);
+        //String stop = br.readLine();
+
+        System.out.println("Adivina el número comprendido entre 0 y 100\nPara terminar escribe \"FIN\".");
+
+        do {
+            System.out.print("\nIntroduce un número: ");
+            respuesta = br.readLine();
+
+            if (!respuesta.equalsIgnoreCase("FIN")) {
+                respuestaint = Integer.parseInt(respuesta);
+                if (respuestaint < aleatorio) {
+                    System.out.println("¡Te has quedado corto!");
+                } else if (respuestaint > aleatorio) {
+                    System.out.println("¡Te has pasado!");
+                } else if (respuestaint == aleatorio) {
+                    System.out.println("¡ACERTASTES!\nFin del juego.");
+                }
+            } else {
+                System.out.println("Has salido\nFin del juego.");
+            }
+        } while (!respuesta.equals("FIN") && respuestaint != aleatorio);
+    }
+}
