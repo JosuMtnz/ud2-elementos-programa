59bf452a9b9f27ecada3e5e89a6f2858daa086ec
Unknown <unknown@example.com>
2017-10-20 13:13:52 +0200

    Eliminada la comprobacion de igualdad y añadido el contador de intentos.

--- 11_acertar_numero/src/com/company/Main.java
+++ 11_acertar_numero/src/com/company/Main.java
@@ -14,6 +14,7 @@ public class Main {
         Random r = new Random();
         int aleatorio = r.nextInt(100);
         int respuestaint = 0;
+        int contador = 0;
         String respuesta = "";
 
         // Mostrar numero generado.
@@ -25,6 +26,7 @@ public class Main {
         do {
             System.out.print("\nIntroduce un número: ");
             respuesta = br.readLine();
+            contador++;
 
             if (!respuesta.equalsIgnoreCase("FIN")) {
                 respuestaint = Integer.parseInt(respuesta);
@@ -32,11 +34,11 @@ public class Main {
                     System.out.println("¡Te has quedado corto!");
                 } else if (respuestaint > aleatorio) {
                     System.out.println("¡Te has pasado!");
-                } else if (respuestaint == aleatorio) {
-                    System.out.println("¡ACERTASTES!\nFin del juego.");
+                } else {
+                    System.out.println("¡ACERTASTES!\nHas necesitado "+contador+" intentos.\nFin del juego.");
                 }
             } else {
-                System.out.println("Has salido\nFin del juego.");
+                System.out.println("Has salido\nHas necesitado "+contador+" intentos.\nFin del juego.");
             }
         } while (!respuesta.equals("FIN") && respuestaint != aleatorio);
     }
